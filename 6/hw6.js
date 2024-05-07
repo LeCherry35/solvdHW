b02632a1f857853da8897c7dc48f2b304a6fdd12
Cherry <lenyamm35@gmail.com>
2024-05-07 15:57:38 +0300

    hw #6

---
+++ 6/hw6.js
@@ -0,0 +1,162 @@
+// Task 1: Quasi-Tagged Templates
+
+const translations = {
+  en: {
+    greet: "Hello",
+    intro: "Welcome to our website",
+  },
+  fr: {
+    greet: "Bonjour",
+    intro: "Bienvenue sur notre site web",
+  },
+};
+
+function localize(stringsArr, ...keys) {
+  const language = "fr"; // language of translation
+  const translation = translations[language];
+
+  return stringsArr.reduce((result, str, i) => {
+    return result + str + (translation[keys[i]] || "");
+  }, "");
+}
+
+const greeting = "greet";
+const introduction = "intro";
+
+const localizedGreeting = localize`${greeting}`;
+const localizedIntroduction = localize`${introduction}`;
+const localizedText = localize`${greeting}! ${introduction}`;
+
+console.log(localizedGreeting);
+console.log(localizedIntroduction);
+console.log(localizedText);
+
+// Task 2: Advanced Tagged Template
+
+function highlightKeywords(template, keywords) {
+    keywords.forEach(keyword => {
+        const regex = new RegExp(keyword, 'gi');
+        template = template.replace(regex, `<span class='highlight'>${keyword}</span>`);
+    });
+    return template;
+}
+
+const keywords = ["JavaScript", "template", "tagged"];
+const template = "Learn JavaScript tagged templates to create custom templateliterals for tagged manipulation.";
+
+const highlighted = highlightKeywords(template, keywords);
+
+console.log(highlighted);
+
+
+// Task 3: Multiline Tagged Template
+
+function multiline(strings, ...values) {
+    const formattedLines = strings.reduce((result, str, i) => {
+        const lines = str.split('\n');
+        let lineNumber = 1
+        lines.forEach((line, index) => {
+                if(line !== '') result.push(`${lineNumber++} ${line}`);
+        });
+        if (i < values.length) {
+            result.push(values[i]);
+        }
+        return result;
+    }, []);
+
+    return formattedLines.join('\n');
+}
+
+const code = multiline`
+function add(a, b) {
+    return a + b;
+}
+`;
+
+console.log(code);
+
+//Task 4: Implementing Debounce Function
+
+function debounce(func, delay) {
+    let timeoutId;
+
+    return function (...args) {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+function debouncedSearch(query) {
+	console.log("Searching for:", query);
+}
+
+const debouncedSearchHandler = debounce(debouncedSearch, 300);
+
+const inputElement = document.getElementById("search-input");
+inputElement.addEventListener("input", event => {
+	debouncedSearchHandler(event.target.value);
+});
+
+// Task 5: Implementing Throttle Function
+
+function throttle(func, interval) {
+    let lastExecution = 0;
+    let timeoutId;
+
+    return function throttled(...args) {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - lastExecution;
+
+        if (elapsedTime >= interval) {
+            clearTimeout(timeoutId);
+
+            func.apply(this, args);
+            lastExecution = currentTime;
+        } else if (!timeoutId) {
+            timeoutId = setTimeout(() => {
+                func.apply(this, args);
+                lastExecution = Date.now();
+                timeoutId = null;
+            }, interval - elapsedTime);
+        }
+    };
+}
+
+function onScroll(event) {
+    console.log("Scroll event:", event);
+}
+
+const throttledScrollHandler = throttle(onScroll, 1000);
+
+window.addEventListener("scroll", throttledScrollHandler);
+
+// Task 6: Currying Function Implementation
+
+function curry (func, arity) {
+    return function curried(...args) {
+        if(args.length >= arity ) {
+            return func(...args)
+        } else {
+            return function (...remainingArgs){
+                return curried(...args, ...remainingArgs)
+
+            }
+        }
+        
+    }
+}
+
+function multiply(a, b, c) {
+    return a * b * c;
+}
+
+const curriedMultiply = curry(multiply, 3);
+
+const step1 = curriedMultiply(2); 
+const step2 = step1(3); 
+const result = step2(4);
+
+console.log("Result:", result); 
